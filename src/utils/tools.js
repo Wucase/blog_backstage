2b14c6f639bb2c88037f7db4d654aba2abcccbf2
wuweiming-lx <13827190867@163.com>
2021-08-14 13:53:13 +0800

    更新菜单

---
+++ src/utils/tools.js
@@ -0,0 +1,19 @@
+import { getMenuList } from "@/api/menu";
+export const getMenuListFun = (type) => {
+  return new Promise((resolve, reject) => {
+    let params = {
+      menuType: type,
+      pageSize: 9999,
+      pageNum: 1,
+      menuStatus: '1'
+    };
+    getMenuList(params).then((res) => {
+      if (res.meta.status === 200) {
+        window.sessionStorage.setItem(type + "Menu", JSON.stringify(res.data.docs))
+        resolve(res.data.docs);
+      }
+    }).catch(err => {
+      reject(err)
+    });
+  })
+}
--- src/views/manager/manager/manager.vue
+++ src/views/manager/manager/manager.vue
@@ -2,38 +2,20 @@
   <div class="home">
     <el-container>
       <el-aside width="250px">
-        <div
-          class="aside-title">
-          <img
-            src="@/assets/images/logo.png"
-            alt="">
+        <div class="aside-title">
+          <img src="@/assets/images/logo.png" alt="">
         </div>
-        <div
-          class="aside-menu">
-          <el-menu router
-            class="el-menu-vertical-demo"
-            background-color="#3E3E3E"
-            text-color="#fff"
-            :default-active="$route.path"
-            active-text-color="#ffd04b"
-            @open="handleOpen"
-            @close="handleClose">
+        <div class="aside-menu">
+          <el-menu router class="el-menu-vertical-demo" background-color="#3E3E3E" text-color="#fff"
+            :default-active="$route.path" active-text-color="#ffd04b" @open="handleOpen" @close="handleClose">
 
-            <el-menu-item
-              @click="toUser">
-              <i
-                class="el-icon-menu"></i>
-              <span
-                slot="title">用户菜单</span>
+            <el-menu-item @click="toUser">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户菜单</span>
             </el-menu-item>
-            <el-menu-item
-              :index="item.menuUrl"
-              v-for="item in menuList"
-              :key="item.id">
-              <i
-                class="el-icon-menu"></i>
-              <span
-                slot="title">{{item.menuName}}</span>
+            <el-menu-item :index="item.menuUrl" v-for="item in menuList" :key="item.id">
+              <i class="el-icon-menu"></i>
+              <span slot="title">{{item.menuName}}</span>
             </el-menu-item>
 
           </el-menu>
@@ -43,10 +25,8 @@
         <el-header>
 
         </el-header>
-        <div
-          class="main-article">
-          <router-view
-            @refleshMenu="refleshMenu" />
+        <div class="main-article">
+          <router-view @refleshMenu="refleshMenu" />
         </div>
 
       </el-main>
@@ -55,90 +35,88 @@
 </template>
 
 <script>
-import { getMenuList } from "@/api/menu";
-export default {
-  name: "Home",
-  components: {},
-  data() {
-    return {
-      menuList: [],
-    };
-  },
-  created() {
-    this.getMenuLists();
-  },
-  methods: {
-    refleshMenu() {
+  import { getMenuListFun } from "@/utils/tools";
+  export default {
+    name: "Home",
+    components: {},
+    data() {
+      return {
+        menuList: [],
+      };
+    },
+    created() {
       this.getMenuLists();
     },
-    getMenuLists() {
-      let params = {
-        menuType: "manager",
-        pageSize: 9999,
-        pageNum: 1,
-        menuStatus: "1",
-      };
-      this.listLoading = true;
-      getMenuList(params).then((res) => {
-        this.listLoading = false;
-        console.log(res);
-        if (res.meta.status === 200) {
-          this.menuList = res.data.docs;
-        }
-      });
+    methods: {
+      async refleshMenu() {
+        this.menuList = await getMenuListFun('manager')
+      },
+      async getMenuLists() {
+        let managerMenu = JSON.parse(window.sessionStorage.getItem("managerMenu"))
+        if (managerMenu) return this.menuList = managerMenu
+        this.menuList = await getMenuListFun('manager')
+      },
+      handleOpen() { },
+      handleClose() { },
+      toUser() {
+        this.$router.push("/users");
+      },
     },
-    handleOpen() {},
-    handleClose() {},
-    toUser() {
-      this.$router.push("/users");
-    },
-  },
-};
+  };
 </script>
 <style lang="scss" scoped>
-.home {
-  width: 100%;
-  height: 100%;
-  .el-container {
+  .home {
     width: 100%;
     height: 100%;
-    .el-aside {
+
+    .el-container {
+      width: 100%;
       height: 100%;
-      background-color: rgb(62, 62, 62);
-    }
-    .el-main {
-      .el-header {
-        padding: 0;
-        margin-bottom: 15px;
-        background-color: #fff;
-        box-sizing: border-box;
+
+      .el-aside {
+        height: 100%;
+        background-color: rgb(62, 62, 62);
       }
-      .main-article {
-        background-color: #fff;
+
+      .el-main {
+        .el-header {
+          padding: 0;
+          margin-bottom: 15px;
+          background-color: #fff;
+          box-sizing: border-box;
+        }
+
+        .main-article {
+          background-color: #fff;
+          padding: 15px;
+          box-sizing: border-box;
+        }
+
+        height: 100%;
+        background-color: rgb(240, 243, 244);
         padding: 15px;
+        padding-top: 0;
         box-sizing: border-box;
       }
-      height: 100%;
-      background-color: rgb(240, 243, 244);
-      padding: 15px;
-      padding-top: 0;
-      box-sizing: border-box;
     }
   }
-}
-.aside-title {
-  width: 100%;
-  height: 60px;
-  background-color: #fff;
-  img {
-    width: 200px;
-    height: 50px;
+
+  .aside-title {
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+
+    img {
+      width: 200px;
+      height: 50px;
+    }
   }
-}
-.aside-menu {
-  margin-top: 20px;
-  .el-menu {
-    border-right: 1px solid #3e3e3e;
+
+  .aside-menu {
+    margin-top: 20px;
+
+    .el-menu {
+      border-right: 1px solid #3e3e3e;
+    }
   }
-}
 </style>
